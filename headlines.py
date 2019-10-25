b897b3b8498602a8ac94c1336da247ba673c66d4
M-Kub <mkubesch@gmail.com>
2019-10-25 10:14:37 +0200

    P30

--- headlines.py
+++ headlines.py
@@ -1,10 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+html = urlopen('https://www.spiegel.de')
 bs = BeautifulSoup(html, 'html.parser')
 
 """for child in bs.find('table', {'id': 'giftList'}).children:
     print(child)"""
-for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
-    print(sibling)
+titels = bs.find_all('span', {'class': 'headline'})
+
+for titel in titels:
+    print(titel.text)
+print(len(titels))
