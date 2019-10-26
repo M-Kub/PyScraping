6b6986e7c3d689e1cd256d27c4e9a8df9e146ac5
M-Kub <mkubesch@gmail.com>
2019-10-26 16:01:40 +0200

    Get links

--- headlines.py
+++ headlines.py
@@ -1,13 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
-html = urlopen('https://www.spiegel.de')
+html = urlopen('https://news.ycombinator.com/news')
 bs = BeautifulSoup(html, 'html.parser')
 
 """for child in bs.find('table', {'id': 'giftList'}).children:
     print(child)"""
-titels = bs.find_all('span', {'class': 'headline'})
+titels = bs.find_all('a',
+    {'href': re.compile('https://*')})
 
 for titel in titels:
-    print(titel.text)
+    print(titel.get('href'))
 print(len(titels))
