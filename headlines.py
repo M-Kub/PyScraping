91ce988a1eaf508d433fb99a21a6dc6d7b26bb61
M-Kub <mkubesch@gmail.com>
2019-10-27 19:59:38 +0100

    Improved code and split it into functions.

--- headlines.py
+++ headlines.py
@@ -5,10 +5,6 @@ import re
 html = urlopen('https://news.ycombinator.com/news')
 bs = BeautifulSoup(html, 'html.parser')
 
-"""for child in bs.find('table', {'id': 'giftList'}).children:
-    print(child)"""
-titels = bs.find_all('a',
-    {'href': re.compile('https://*')})
 
 for titel in titels:
     print(titel.get('href'))
