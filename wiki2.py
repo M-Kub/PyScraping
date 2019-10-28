6004cf21650ead261cb427d51b5b486b58564e2a
M-Kub <mkubesch@gmail.com>
2019-10-28 14:59:20 +0100

    clickabe links now(still in need of improvement)

--- wiki2.py
+++ wiki2.py
@@ -11,7 +11,8 @@ random.seed(datetime.datetime.now())
 def getLinks(articleURL):
     html = urlopen('https://en.wikipedia.org{}'.format(articleURL))
     bs = BeautifulSoup(html, 'html.parser')
-    article = bs.find_all('a', {'href': re.compile('^(/wiki/)((?!:).)*$')})
+    article = bs.find('div', {'id': 'bodyContent'}).find_all('a',
+                        href=re.compile('^(/wiki/)((?!:).)*$'))
     return article
 
 
@@ -19,7 +20,7 @@ def adresse(self):
     links = getLinks(self)
     while len(links) > 0:
         newArticle = links[random.randint(0, len(links) - 1)].attrs['href']
-        print(newArticle)
+        print('https://en.wikipedia.org' + newArticle)
         links = getLinks(self)
 
 
